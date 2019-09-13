ead80e9fe664376d5e93acc87c326045f424df60
Ramesh Sivaraman <ramesh.sivaraman@percona.com>
2019-09-13 15:11:45 +0530

    Startup script updates

--- util/ps_startup.py
+++ util/ps_startup.py
@@ -25,7 +25,7 @@ class StartPerconaServer:
             the availability of mysqld binary file.
         """
         # kill existing mysqld process
-        os.system("ps -ef | grep '" + self.workdir + "/ps[0-9].cnf"
+        os.system("ps -ef | grep '" + self.workdir + "/conf/ps[0-9].cnf"
                   " | grep -v grep | awk '{print $2}' | xargs kill -9 >/dev/null 2>&1")
         if not os.path.exists(self.workdir + '/log'):
             os.mkdir(self.workdir + '/log')
