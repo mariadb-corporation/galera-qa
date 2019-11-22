4f22df2df7e4aa7936cb2ba7dd0b8c11c3c287af
rameshvs02 <rameshvs02@gmail.com>
2019-11-22 15:25:51 +0530

    Added upgrade testcases (Mix of 5.7 and 8.0 nodes) to upgrade script

--- suite/upgrade/pxc_upgrade.py
+++ suite/upgrade/pxc_upgrade.py
@@ -4,6 +4,7 @@ import sys
 import argparse
 import subprocess
 import time
+import shutil
 from datetime import datetime
 cwd = os.path.dirname(os.path.realpath(__file__))
 parent_dir = os.path.normpath(os.path.join(cwd, '../../'))
@@ -58,6 +59,75 @@ class PXCUpgrade:
         result = dbconnection_check.connection_check()
         utility_cmd.check_testcase(result, "Database connection")
 
+    def startup_check(self, cluster_node):
+        """ This method will check the node
+            startup status.
+        """
+        query_cluster_status = PXC_LOWER_BASE + '/bin/mysql --user=root --socket=' + \
+            WORKDIR + '/node' + str(cluster_node) + \
+            '/mysql.sock -Bse"show status like \'wsrep_local_state_comment\';"' \
+            ' 2>/dev/null | awk \'{print $2}\''
+        ping_query = PXC_LOWER_BASE + '/bin/mysqladmin --user=root --socket=' + \
+            WORKDIR + '/node' + str(cluster_node) + \
+            '/mysql.sock ping > /dev/null 2>&1'
+        for startup_timer in range(300):
+            time.sleep(1)
+            cluster_status = os.popen(query_cluster_status).read().rstrip()
+            if cluster_status == 'Synced':
+                utility_cmd.check_testcase(0, "Node startup is successful")
+                break
+            if startup_timer > 298:
+                utility_cmd.check_testcase(0, "Warning! Node is not synced with cluster. "
+                                              "Check the error log to get more info")
+                ping_check = subprocess.call(ping_query, shell=True, stderr=subprocess.DEVNULL)
+                ping_status = ("{}".format(ping_check))
+                if int(ping_status) == 0:
+                    utility_cmd.check_testcase(int(ping_status), "Node startup is successful "
+                                                                 "(Node status:" + cluster_status + ")")
+                    break  # break the loop if mysqld is running
+
+    def start_upper_version(self):
+        # Start PXC cluster for upgrade test
+        shutil.copy(WORKDIR + '/conf/node3.cnf',
+                    WORKDIR + '/conf/node4.cnf')
+        query = PXC_LOWER_BASE + '/bin/mysql --user=root --socket=' + WORKDIR + \
+            '/node3/mysql.sock -Bse"show variables like \'wsrep_cluster_address\';"' \
+            ' 2>/dev/null | awk \'{print $2}\''
+        wsrep_cluster_addr = os.popen(query).read().rstrip()
+        query = PXC_LOWER_BASE + "/bin/mysql --user=root --socket=" + \
+                WORKDIR + '/node3/mysql.sock -Bse"select @@port" 2>&1'
+        port_no = os.popen(query).read().rstrip()
+        wsrep_port_no = int(port_no) + 108
+        port_no = int(port_no) + 100
+        os.system("sed -i 's#node3#node4#g' " + WORKDIR + '/conf/node4.cnf')
+        os.system("sed -i '/wsrep_sst_auth=root:/d' " + WORKDIR + '/conf/node4.cnf')
+        os.system("sed -i  '0,/^[ \\t]*wsrep_cluster_address[ \\t]*=.*$/s|" 
+                  "^[ \\t]*wsrep_cluster_address[ \\t]*=.*$|wsrep_cluster_address="
+                  + wsrep_cluster_addr + "127.0.0.1:" + str(wsrep_port_no) + "|' "
+                  + WORKDIR + '/conf/node4.cnf')
+        os.system("sed -i  '0,/^[ \\t]*port[ \\t]*=.*$/s|"
+                  "^[ \\t]*port[ \\t]*=.*$|port="
+                  + str(port_no) + "|' " + WORKDIR + '/conf/node4.cnf')
+        os.system('sed -i  "0,/^[ \\t]*wsrep_provider_options[ \\t]*=.*$/s|'
+                  "^[ \\t]*wsrep_provider_options[ \\t]*=.*$|wsrep_provider_options="
+                  "'gmcast.listen_addr=tcp://127.0.0.1:" + str(wsrep_port_no) + "'"
+                  '|" ' + WORKDIR + '/conf/node4.cnf')
+        os.system("sed -i  '0,/^[ \\t]*server_id[ \\t]*=.*$/s|"
+                  "^[ \\t]*server_id[ \\t]*=.*$|server_id="
+                  "14|' " + WORKDIR + '/conf/node4.cnf')
+        create_startup = 'sed  "s#' + PXC_LOWER_BASE + '#' + PXC_UPPER_BASE + \
+                         '#g" ' + WORKDIR + '/log/startup3.sh > ' + \
+                         WORKDIR + '/log/startup4.sh'
+        os.system(create_startup)
+        os.system("sed -i 's#node3#node4#g' " + WORKDIR + '/log/startup4.sh')
+        os.system("rm -rf " + WORKDIR + '/node4')
+        os.mkdir(WORKDIR + '/node4')
+        upgrade_startup = "bash " + WORKDIR + \
+                          '/log/startup4.sh'
+        result = os.system(upgrade_startup)
+        utility_cmd.check_testcase(result, "Starting PXC-8.0 cluster node4 for upgrade testing")
+        self.startup_check(4)
+
     def sysbench_run(self, node1_socket, db, upgrade_type):
         # Sysbench dataload for consistency test
         sysbench_node1 = sysbench_run.SysbenchRun(PXC_LOWER_BASE, WORKDIR,
@@ -101,33 +171,6 @@ class PXCUpgrade:
             result = sysbench_node3.sysbench_oltp_read_only(db, SYSBENCH_TABLE_COUNT, SYSBENCH_THREADS,
                                                             SYSBENCH_NORMAL_TABLE_SIZE, 1000, 'Yes')
             utility_cmd.check_testcase(result, "Initiated sysbench readonly run on node3")
-
-    def startup_check(self, cluster_node):
-        """ This method will check the node
-            startup status.
-        """
-        query_cluster_status = PXC_LOWER_BASE + '/bin/mysql --user=root --socket=' + \
-            WORKDIR + '/node' + str(cluster_node) + \
-            '/mysql.sock -Bse"show status like \'wsrep_local_state_comment\';"' \
-            ' 2>/dev/null | awk \'{print $2}\''
-        ping_query = PXC_LOWER_BASE + '/bin/mysqladmin --user=root --socket=' + \
-            WORKDIR + '/node' + str(cluster_node) + \
-            '/mysql.sock ping > /dev/null 2>&1'
-        for startup_timer in range(300):
-            time.sleep(1)
-            cluster_status = os.popen(query_cluster_status).read().rstrip()
-            if cluster_status == 'Synced':
-                utility_cmd.check_testcase(0, "Node startup is successful")
-                break
-            if startup_timer > 298:
-                utility_cmd.check_testcase(0, "Warning! Node is not synced with cluster. "
-                                              "Check the error log to get more info")
-                ping_check = subprocess.call(ping_query, shell=True, stderr=subprocess.DEVNULL)
-                ping_status = ("{}".format(ping_check))
-                if int(ping_status) == 0:
-                    utility_cmd.check_testcase(int(ping_status), "Node startup is successful "
-                                                                 "(Node status:" + cluster_status + ")")
-                    break  # break the loop if mysqld is running
 
     def rolling_upgrade(self, upgrade_type):
         """ This function will upgrade
@@ -278,3 +321,20 @@ upgrade_qa.startup('wsrep_extra')
 rqg_dataload = rqg_datagen.RQGDataGen(PXC_LOWER_BASE, WORKDIR, USER)
 rqg_dataload.pxc_dataload(WORKDIR + '/node1/mysql.sock')
 upgrade_qa.rolling_upgrade('readwrite')
+if int(version) > int("080000"):
+    print('--------------------------------------------------------------------------------------')
+    print(datetime.now().strftime("%H:%M:%S ") + "PXC Upgrade test : Mix of PXC-" +
+          lower_version + " and PXC-" + upper_version + "(without active workload)")
+    print('--------------------------------------------------------------------------------------')
+    upgrade_qa = PXCUpgrade()
+    upgrade_qa.startup()
+    upgrade_qa.start_upper_version()
+    print('--------------------------------------------------------------------------------------')
+    print(datetime.now().strftime("%H:%M:%S ") + "PXC Upgrade test : Mix of PXC-" +
+          lower_version + " and PXC-" + upper_version + "(with active read/write workload)")
+    print('--------------------------------------------------------------------------------------')
+    upgrade_qa.startup('wsrep_extra')
+    rqg_dataload = rqg_datagen.RQGDataGen(PXC_LOWER_BASE, WORKDIR, USER)
+    rqg_dataload.pxc_dataload(WORKDIR + '/node1/mysql.sock')
+    upgrade_qa.rolling_upgrade('readwrite')
+    upgrade_qa.start_upper_version()
