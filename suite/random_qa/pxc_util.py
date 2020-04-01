d9405249755b0fd311ea5f6dafed8ae590320da3
rameshvs02 <rameshvs02@gmail.com>
2020-04-01 10:46:59 +0530

    renamed start_pxc.py to pxc_util.py. Added shutdown function

--- suite/random_qa/pxc_util.py
+++ suite/random_qa/pxc_util.py
@@ -16,9 +16,14 @@ utility_cmd = utility.Utility()
 utility_cmd.check_python_version()
 
 # Read argument
-parser = argparse.ArgumentParser(prog='PXC random mysqld option test', usage='%(prog)s [options]')
+parser = argparse.ArgumentParser(prog='PXC Utility', usage='%(prog)s [options]')
 parser.add_argument('-e', '--encryption-run', action='store_true',
                     help='This option will enable encryption options')
+parser.add_argument('--start', action='store_true',
+                    help='Start PXC nodes')
+parser.add_argument('--stop', action='store_true',
+                    help='Stop PXC nodes')
+
 args = parser.parse_args()
 if args.encryption_run is True:
     encryption = 'YES'
@@ -26,7 +31,7 @@ else:
     encryption = 'NO'
 
 
-class StartPXC:
+class PXCUtil:
     def start_pxc(self):
         # Start PXC cluster
         dbconnection_check = db_connection.DbConnection(USER, WORKDIR + '/node1/mysql.sock')
@@ -41,7 +46,7 @@ class StartPXC:
             utility_cmd.check_testcase(result, "Configuration file creation")
         result = server_startup.initialize_cluster()
         utility_cmd.check_testcase(result, "Initializing cluster")
-        result = server_startup.start_cluster('--max-connections=1500')
+        result = server_startup.start_cluster('--max-connections=1500 ')
         utility_cmd.check_testcase(result, "Cluster startup")
         result = dbconnection_check.connection_check()
         utility_cmd.check_testcase(result, "Database connection")
@@ -53,10 +58,23 @@ class StartPXC:
             # return 1
             print("ERROR!: Could not create test database.")
             exit(1)
+        utility_cmd.check_testcase(0, "PXC connection string")
+        for i in range(1, int(NODE) + 1):
+            print('\t' + BASEDIR + '/bin/mysql --user=root --socket=' +
+                  WORKDIR + '/node' + str(i) + '/mysql.sock')
+
+    def stop_pxc(self):
+        # Stop PXC cluster
+        for i in range(int(NODE), 0, -1):
+            shutdown_node = BASEDIR + '/bin/mysqladmin --user=root --socket=' + \
+                        WORKDIR + '/node' + str(i) + '/mysql.sock shutdown > /dev/null 2>&1'
+            result = os.system(shutdown_node)
+            utility_cmd.check_testcase(result, "PXC: shutting down cluster node" + str(i))
 
 
-print("--------------------")
-print("Start PXC Cluster   ")
-print("--------------------")
-start_pxc = StartPXC()
-start_pxc.start_pxc()
+pxc_util = PXCUtil()
+if args.start is True:
+    pxc_util.start_pxc()
+
+if args.stop is True:
+    pxc_util.stop_pxc()
